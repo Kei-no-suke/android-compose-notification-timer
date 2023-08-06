66a8c461bba923726785070c23658c995d23983c
Kei-no-suke <55536596+Kei-no-suke@users.noreply.github.com>
2023-08-06 16:06:00 +0900

    Added notification feature

--- app/src/main/java/com/example/notificationtimer/MainActivity.kt
+++ app/src/main/java/com/example/notificationtimer/MainActivity.kt
@@ -1,20 +1,54 @@
 package com.example.notificationtimer
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.example.notificationtimer.ui.theme.NotificationTimerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val requestPermissionLauncher = registerForActivityResult(
+                ActivityResultContracts.RequestPermission(),
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "FCM can't post notifications without POST_NOTIFICATIONS permission",
+                        Toast.LENGTH_LONG,
+                    ).show()
+                }
+            }
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
         setContent {
             NotificationTimerTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +56,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NotificationScreen(this)
                 }
             }
         }
@@ -30,17 +64,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotificationTimerTheme {
-        Greeting("Android")
+fun NotificationScreen(context: Context, modifier: Modifier = Modifier) {
+    Column {
+        Button(onClick = { makeNotification("Notification Timer", "Notification", context) }) {
+            Text("Notification")
+        }
     }
 }
---
+++ app/src/main/java/com/example/notificationtimer/Notification.kt
@@ -0,0 +1,55 @@
+package com.example.notificationtimer
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+const val CHANNEL_ID = "NOTIFICATION_TIMER"
+
+const val NOTIFICATION_ID = 1
+
+fun makeNotification(title: String, contentText: String, context: Context) {
+    val name = "Notification Timer"
+    val importance = NotificationManager.IMPORTANCE_DEFAULT
+    val description = "Displays notifications when time is up."
+    val channel = NotificationChannel(
+        CHANNEL_ID,
+        name,
+        importance
+    )
+    channel.description = description
+    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    notificationManager.createNotificationChannel(channel)
+
+    val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setContentTitle(title)
+        .setContentText(contentText)
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .setAutoCancel(true)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            with(NotificationManagerCompat.from(context)) {
+                // notificationId is a unique int for each notification that you must define
+                notify(NOTIFICATION_ID, builder.build())
+            }
+        }
+    }else{
+        with(NotificationManagerCompat.from(context)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(NOTIFICATION_ID, builder.build())
+        }
+    }
+}
